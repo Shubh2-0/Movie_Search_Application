9fe0e255080f3ec9229b74d8f189a8e414a8e458
shubham bhati <112773220+Shubh2-0@users.noreply.github.com>
2023-01-02 02:33:28 +0530

    update

--- Movie_App/component/slider.js
+++ Movie_App/component/slider.js
@@ -3,7 +3,7 @@ function slider(){
    let slide=document.getElementById("slide")
 slide.innerHTML=null
 
-let arr=["Slide1.png","Slide2.png","Slide3.png","Slide4.png","Slide5.png","Slide6.png","Slide7.png","Slide8.png","Slide9.png","Slide10.png","Slide11.png","Slide12.png","Slide13.png"]
+let arr=[".slider/Slide1.png","Slide2.png","Slide3.png","Slide4.png","Slide5.png","Slide6.png","Slide7.png","Slide8.png","Slide9.png","Slide10.png","Slide11.png","Slide12.png","Slide13.png"];
 
 
 let num=Math.floor( Math.random()*13)
