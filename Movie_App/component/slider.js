cc9372d2c5646d2484e437ec28cb8438e225d8bc
shubham bhati <112773220+Shubh2-0@users.noreply.github.com>
2023-01-30 00:32:22 +0530

    created image folder for slider images

--- Movie_App/component/slider.js
+++ Movie_App/component/slider.js
@@ -3,15 +3,17 @@ function slider(){
    let slide=document.getElementById("slide")
 slide.innerHTML=null
 
-let arr=["Slide1.png","Slide2.png","Slide3.png","Slide4.png","Slide5.png","Slide6.png","Slide7.png","Slide8.png","Slide9.png","Slide10.png","Slide11.png","Slide12.png","Slide13.png"];
+let arr2=["slider_images/Slide1.png","slider_images/Slide2.png","slider_images/Slide3.png","slider_images/Slide4.png","slider_images/Slide5.png","slider_images/Slide6.png","slider_images/Slide7.png","slider_images/Slide8.png","slider_images/Slide9.png","slider_images/Slide10.png","slider_images/Slide11.png","slider_images/Slide12.png","slider_images/Slide13.png"];
+
+// let arr2=["https://scontent.fbho3-1.fna.fbcdn.net/v/t39.30808-6/327474399_2458166661002204_3456931140616213573_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=0debeb&_nc_ohc=x5DaWfllbswAX8ShEgE&_nc_ht=scontent.fbho3-1.fna&oh=00_AfDfzrOC_EH1yeu6RUzlQ26WYC3c4UMgIJTVwE3I6yxBcg&oe=63DC014B","https://www.facebook.com/photo/?fbid=116337848026314&set=pob.100089502985491","https://www.facebook.com/photo?fbid=116337391359693&set=pob.100089502985491","https://www.facebook.com/photo?fbid=116337401359692&set=pob.100089502985491","https://www.facebook.com/photo/?fbid=116337644693001&set=pob.100089502985491","https://www.facebook.com/photo/?fbid=116337601359672&set=pob.100089502985491","https://www.facebook.com/photo/?fbid=116337584693007&set=pob.100089502985491","https://www.facebook.com/photo/?fbid=116337521359680&set=pob.100089502985491","https://www.facebook.com/photo?fbid=116337498026349&set=pob.100089502985491","https://www.facebook.com/photo?fbid=116337464693019&set=pob.100089502985491","https://www.facebook.com/photo?fbid=116337421359690&set=pob.100089502985491"]
 
 
 
-let num=Math.floor( Math.random()*arr.length)
+let num=Math.floor( Math.random()*arr2.length)
 
-let poster=arr[num]
+let poster=arr2[num]
 
-console.log(num)
+// console.log(num)
 
 let img=document.createElement("img")
 
--- Movie_App/script/index.js
+++ Movie_App/script/index.js
@@ -12,7 +12,7 @@ import sl from "../component/slider.js"
 
 
 
-setInterval(sl,2000)
+setInterval(sl,3000)
 
 
 
